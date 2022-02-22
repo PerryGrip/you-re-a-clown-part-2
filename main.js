bd40055e8c397bd8dcc26a2ede91d7d19cbcc128
PerryGrip <97786175+PerryGrip@users.noreply.github.com>
2022-02-22 18:30:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function preload(){
+}
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log('poseNet is Initalized');
+}
+
+noseX=0;
+noseY=0;
+
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x -30;
+        noseY = results[0].pose.nose.y -20;
+        image(clown_nose, noseX, noseY, 60, 60);
+    }
+}
+
+function draw(){
+image(video, 0, 0, 300, 300);
+image(clown_nose, noseX, noseY, 60, 60);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+    
+}
+
+function preload(){
+clown_nose = loadImage('https://i.postimg.cc/Pxf0W3Yc/a-nose.png');
+}
